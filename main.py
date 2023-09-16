72dab5b0a4e52bd8b7032c9e2eed158df42945c4
Patrick Soares de souza silva <patrick.soares@iholdbank.digital>
2023-09-16 16:54:59 -0300

    main

--- main.py
+++ main.py
@@ -16,11 +16,47 @@ app.add_middleware(
 def index():
     return {'message': 'Hello World'}
 
+"""
+{'object': 'whatsapp_business_account',
+'entry': [
+    {'id': '118349154557213', 
+    'changes': [
+         {'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '15550847474', 'phone_number_id': '115759418151225'}, 
+        'contacts': [{'profile': {'name': 'Patrick'}, 'wa_id': '5527995258068'}], 
+    'messages': [
+        {'from': '5527995258068', 'id': 'wamid.HBgNNTUyNzk5NTI1ODA2OBUCABIYIEVCNUY2RERDRTc2QTAzMTNCQTY2NTk0NUJEMjJGMzRDAA==', 'timestamp': '1694891965', 'text': {'body': 'Oi'}, 'type': 'text'}
+        ]
+     },
+     'field': 'messages'}
+     ]
+    }
+    ]
+    }
+"""
+
 @app.post('/webhook')
 def webhook(body:dict,req: Request):
-    value = {}
-    print(body)
-    if req and "body" in req and "entry" in body and len(body["entry"]) > 0 \
+    entry = body.get('entry',[])
+    if len(entry) > 0:
+        entry = entry[0]
+    id = entry.get('id',None)
+    changes = entry.get('changes',[])
+    if len(changes) > 0:
+        changes = changes[0]
+    messages = changes.get('messages',[])
+    if len(messages) > 0:
+        messages = messages[0]
+    from_ = messages.get('from',None)
+    message_id = messages.get('id',None)
+    message = messages.get('text',{}).get('body',None)
+    type = messages.get('type',None)
+    print(from_,message_id,message,type,id)
+    
+ 
+
+        
+        
+    """if req and "body" in req and "entry" in body and len(body["entry"]) > 0 \
         and "changes" in body["entry"][0] and len(body["entry"][0]["changes"]) > 0:
         value = body["entry"][0]["changes"][0].get("value")
 
@@ -32,8 +68,8 @@ def webhook(body:dict,req: Request):
             location = value["messages"][0].get("location")
             interaction = value["messages"][0].get("interaction")
             if message:
-                WPService().send_message({'to':destination,'message':'foi'},id_)
+                WPService().send_message({'to':destination,'message':'foi'},id_)"""
                 
             
 
-    return int(req.query_params['hub.challenge'])
+    return {'message': 'Hello World'}
